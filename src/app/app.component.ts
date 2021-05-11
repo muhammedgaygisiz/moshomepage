f91c1fcedc52c452d69738e65d4842b7f7139f77
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2021-05-11 21:02:47 +0200

    Some styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-progressbar></app-progressbar>
     <router-outlet></router-outlet>
   `,
   styles: ['']
